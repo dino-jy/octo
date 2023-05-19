824bd2ab559e90100b2c1285bfd46e60de0ed194
Turtle <115870070+dino-jy@users.noreply.github.com>
2023-05-19 22:11:41 +0600

    Create clbot.py

---
+++ clbot.py
@@ -0,0 +1,108 @@
+import logging 
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Token 
+TOKEN = '6276398338:AAGmAh2_g8gMc-JS-Lhmif2GVhPzEARpwYQ'
+
+# Database to store user info 
+db = {}
+
+# Admin info
+ADMINS = [6270279846]  # Replace with admin user_ids
+
+# Currency name 
+CURRENCY = 'uwu' 
+
+# Subscription plans 
+plans = {
+    'daily': {
+        'price': 5,
+        'period': 1 
+    },
+    'weekly': {
+        'price': 25,
+        'period': 7
+    }, 
+    'monthly': {
+        'price': 100,
+        'period': 30
+    } 
+}
+
+# Initialize bot 
+updater = Updater(TOKEN, use_context=True)
+bot = updater.bot
+dispatcher = updater.dispatcher
+
+# Welcome message 
+def start(update, context):
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
+    name = update.message.from_user.first_name
+    db[chat_id] = {
+        'user_id': user_id,
+        'name': name,
+        'bal': 0,
+        'sub': None 
+    }
+    text = f'Hello {name}! Welcome to the {bot.name} bot. Type /help to know more.'
+    bot.send_message(chat_id, text)
+
+# Help message 
+def help(update, context):
+   ...
+
+#fortune command handler 
+def fortune(update, context):
+    fortunes = [
+        'You will have a bright future.',
+        'Things will get better soon.', 
+        'Keep working hard. Success is on its way.'
+    ]
+    fortune = fortunes[randint(0, len(fortunes)-1)]
+    update.message.reply_text(fortune)
+
+# Admin features
+def ban(update, context):
+    if update.message.from_user.id not in ADMINS:
+        update.message.reply_text('You are not authorized to use admin commands!')
+        return
+    ...  
+# User and admin chat 
+def chat(update, context):
+   ... 
+# Subscription plans 
+def subscribe(update, context):
+   ...
+#Send uwu (currency) 
+def send(update, context):
+   ...
+# Balance and other user info  
+def profile(update, context):
+   ...
+
+# Error handler
+def error(update, context):
+   ...
+
+# Add handlers 
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help)) 
+dispatcher.add_handler(CommandHandler('fortune', fortune))
+dispatcher.add_handler(CommandHandler('ban', ban, pass_args=True))  
+dispatcher.add_handler(MessageHandler(Filters.text, chat))
+dispatcher.add_handler(CommandHandler('subscribe', subscribe))
+dispatcher.add_handler(CommandHandler('send', send, pass_args=True))
+dispatcher.add_handler(CommandHandler('profile', profile))
+dispatcher.add_error_handler(error)
+
+# Run the bot
+updater.start_polling()
+updater.idle()
