9bcf889f0f5ecc558400385588f36c55a18b6dbc
Turtle <115870070+dino-jy@users.noreply.github.com>
2023-05-19 19:01:19 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,54 @@
+from queue import Queue
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import redis
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(name)
+
+# Telegram bot token
+BOT_TOKEN = '6276398338:AAGmAh2_g8gMc-JS-Lhmif2GVhPzEARpwYQ'
+
+# Redis host and port
+REDIS_HOST = 'redis-16860.c16.us-east-1-3.ec2.cloud.redislabs.com'
+REDIS_PORT = 16860
+
+# Database number for bot
+DB_NUM = 0
+
+update_queue = Queue()
+updater = Updater(BOT_TOKEN, update_queue)
+dispatcher = updater.dispatcher
+
+# Create Redis connection
+redis_conn = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=DB_NUM)
+
+# Utility functions to get/save user from database
+# ...
+
+# Start command
+def start(update, context):
+    # ...
+
+# Buy, gift and chat command handlers
+# ...
+
+# Error handler
+def error(update, context):
+    ...
+
+# Add handlers
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('buy', buy))
+dispatcher.add_handler(CommandHandler('gift', gift))
+dispatcher.add_handler(CommandHandler('chat', chat))
+dispatcher.add_error_handler(error)
+
+# Start the Bot
+updater.start_polling()
+
+# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+# SIGTERM or SIGABRT
+updater.idle()
